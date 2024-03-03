1403c351e0b0078c86b9d96fbdb4902c34b1eef2
jrmohsinkhan <141910380+jrmohsinkhan@users.noreply.github.com>
2024-03-03 12:09:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const display = document.getElementById('display');
+
+const appendToDisplay = value => {
+    display.value += value;
+};
+
+const clearDisplay = () => {
+    display.value = '';
+};
+
+const calculate = () => {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+};
